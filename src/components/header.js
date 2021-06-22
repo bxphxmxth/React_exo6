1c9cd133dbdd39e13f28821392279768b8bd12da
bxphxmxth <fushs@hotmail.fr>
2021-06-22 12:19:47 +0200

    exo6

---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+class Header extends React.Component{
+
+     render(){
+          return (
+               <div>
+                    Ceci est le header
+               </div>
+          )
+     }
+}
+
+export default Header;
